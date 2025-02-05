9c680bd142f1382d5c219fa6c089d0c688571a87
Suraj15032003 <suraj1412.be21@chitkara.edu.in>
2025-02-05 10:30:27 +0530

    [Suraj] updating empy file with attd code

--- emp.py
+++ emp.py
@@ -1,1 +1,13 @@
-print("welcome to code ")
+#print("welcome to code ")
+import random
+
+def emp_attd():
+    randnumber = random.random()
+    threshold = 0.5
+    if randnumber < threshold:
+        print("absent")
+        return 0
+    else:
+        print("present")
+        return 1
+emp_attd()
